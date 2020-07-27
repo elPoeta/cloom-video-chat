8df1207b6fcae4a3b87d82d43c2d53cba945f884
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-07-27 18:26:18 -0300

    add video on client

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,1 +1,20 @@
-console.log('app...')
+const gridVideo = document.querySelector('#gridVideo');
+
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true
+}).then(stream => {
+  addVideoStream(myVideo, stream);
+})
+
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream
+  video.addEventListener('loadedmetadata', () => {
+    video.play()
+  })
+  gridVideo.append(video)
+}
